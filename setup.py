f8f2558aeb2fd80635c15bf756a5c3e105363bea
Camille Marini <camille.marini@gmail.com>
2018-07-27 11:03:11 +0200

    packaging of substramodel

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(name='substratools',
+      version='0.0',
+      description='Python tools to submit algo on the Substra platform',
+      author='Camille',
+      packages=['substratools'],
+      install_requires=['numpy'],
+      classifiers=[
+          'Programming Language :: Python :: 3',
+          'Topic :: Scientific/Engineering'
+      ],
+      zip_safe=False)
