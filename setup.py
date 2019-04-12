e0e71f1ee0c7fec92a8cea5e5fbfc4c5b10b881c
Samuel <samuel.lesuffleur@owkin.com>
2019-04-12 16:59:49 +0200

    Revert "Update setup.py"
    This reverts commit 5f682f0ba51f0bad1c10c292d4b4e3be003893c4.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     name='substratools',
     version='0.0',
     description='Python tools to submit algo on the Substra platform',
-    author='Camille Marini',
+    author='Camille',
     packages=find_packages(),
     install_requires=[
         'click',
