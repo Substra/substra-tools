bf86a9199c56e586a938a5bfb308e527ef06bc13
Samuel <samuel.lesuffleur@owkin.com>
2019-04-17 11:09:56 +0200

    add tests for 100% coverage

--- tests/test_metrics.py
+++ tests/test_metrics.py
@@ -1,7 +1,9 @@
 import json
+import sys
 
 from substratools import metrics
 from substratools.workspace import Workspace
+from substratools.utils import import_module
 
 import pytest
 
@@ -13,6 +15,19 @@ def write_pred_file():
     with open(workspace.pred_filepath, 'w') as f:
         json.dump(data, f)
     return workspace.pred_filepath, data
+
+
+@pytest.fixture
+def load_metrics_module():
+    code = """
+from substratools import Metrics
+class DummyMetrics(Metrics):
+    def score(self, y_true, y_pred):
+        return y_true + y_pred['key']
+"""
+    import_module('metrics', code)
+    yield
+    del sys.modules['metrics']
 
 
 @pytest.fixture(autouse=True)
@@ -36,6 +51,6 @@ def test_score():
     assert s == 'ypred'
 
 
-def test_execute():
-    s = metrics._execute(DummyMetrics())
+def test_execute(load_metrics_module):
+    s = metrics.execute()
     assert s == 'ypred'
