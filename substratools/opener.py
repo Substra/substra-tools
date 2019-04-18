62edf993a801bcd7811fcb30f326344839536627
Samuel <samuel.lesuffleur@owkin.com>
2019-04-18 10:12:24 +0200

    opener: get_X/get_y take as input list of folders

--- substratools/opener.py
+++ substratools/opener.py
@@ -1,4 +1,5 @@
 import abc
+import os
 import types
 
 from substratools import workspace, utils
@@ -8,13 +9,13 @@ class Opener(abc.ABC):
     """Dataset opener abstract base class."""
 
     @abc.abstractmethod
-    def get_X(self, folder):
-        """Load feature data from folder."""
+    def get_X(self, folders):
+        """Load feature data from data sample folders."""
         raise NotImplementedError
 
     @abc.abstractmethod
-    def get_y(self, folder):
-        """Load labels."""
+    def get_y(self, folders):
+        """Load labels from data sample folders."""
         raise NotImplementedError
 
     @abc.abstractmethod
@@ -50,17 +51,24 @@ class OpenerWrapper(object):
         self._interface = opener
         self._workspace = workspace.Workspace()
 
+    @property
+    def data_folder_paths(self):
+        rootpath = self._workspace.data_folder
+        folders = [os.path.join(rootpath, subfolder)
+                   for subfolder in os.listdir(rootpath)]
+        return folders
+
     def get_X(self, dry_run=False):
         if dry_run:
             return self._interface.fake_X()
         else:
-            return self._interface.get_X(self._workspace.data_folder)
+            return self._interface.get_X(self.data_folder_paths)
 
     def get_y(self, dry_run=False):
         if dry_run:
             return self._interface.fake_y()
         else:
-            return self._interface.get_y(self._workspace.data_folder)
+            return self._interface.get_y(self.data_folder_paths)
 
     def get_pred(self):
         return self._interface.get_pred(self._workspace.pred_filepath)
--- tests/test_opener.py
+++ tests/test_opener.py
@@ -44,9 +44,9 @@ def test_load_opener_as_module(tmp_cwd):
     script = """
 def _helper():
     pass
-def get_X(folder):
+def get_X(folders):
     return 'X'
-def get_y(folder):
+def get_y(folders):
     return 'y'
 def fake_X():
     return 'fakeX'
@@ -68,9 +68,9 @@ def test_load_opener_as_class(tmp_cwd):
     script = """
 from substratools import Opener
 class MyOpener(Opener):
-    def get_X(self, folder):
+    def get_X(self, folders):
         return 'Xclass'
-    def get_y(self, folder):
+    def get_y(self, folders):
         return 'yclass'
     def fake_X(self):
         return 'fakeX'
@@ -85,4 +85,36 @@ class MyOpener(Opener):
     import_module('opener', script)
 
     o = load_from_module()
+    assert o.get_X() == 'Xclass'
+
+
+def test_opener_check_folders(tmp_cwd):
+    script = """
+from substratools import Opener
+class MyOpener(Opener):
+    def get_X(self, folders):
+        assert len(folders) == 5
+        return 'Xclass'
+    def get_y(self, folders):
+        return 'yclass'
+    def fake_X(self):
+        return 'fakeX'
+    def fake_y(self):
+        return 'fakey'
+    def get_pred(self, path):
+        return 'pred'
+    def save_pred(self, y_pred, path):
+        return 'pred'
+"""
+
+    import_module('opener', script)
+
+    o = load_from_module()
+
+    # create some data folders
+    data_root_path = o._workspace.data_folder
+    data_paths = [os.path.join(data_root_path, str(i)) for i in range(5)]
+    [os.makedirs(p) for p in data_paths]
+
+    o.data_folders = data_paths
     assert o.get_X() == 'Xclass'
