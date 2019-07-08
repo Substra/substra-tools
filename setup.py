768ad6643825db82fc38f4a17535c4906e657d6f
Samuel <samuel.lesuffleur@owkin.com>
2019-07-08 12:02:24 +0200

    setup.py: consistency in quotes: use single quotes

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(
         'Programming Language :: Python :: 3',
         'Topic :: Scientific/Engineering'
     ],
-    setup_requires=["pytest-runner"],
-    tests_require=["pytest", "pytest-cov"],
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest', 'pytest-cov', 'pytest-mock'],
     zip_safe=False
 )
