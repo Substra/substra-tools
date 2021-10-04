932d5d851d9ef7d87c7f89d5553e5f1152cd8d4d
Maria Telenczuk <maja_ka@hotmail.com>
2021-10-04 14:19:33 +0200

    feat: add substratools.__version__ (#9)

--- substratools/__init__.py
+++ substratools/__init__.py
@@ -2,8 +2,12 @@ from . import algo, metrics, opener
 from .algo import Algo, CompositeAlgo, AggregateAlgo
 from .metrics import Metrics
 from .opener import Opener
+from substratools.__version__ import __version__
 
 
-__all__ = [algo, Algo, CompositeAlgo, AggregateAlgo,
-           metrics, Metrics,
-           opener, Opener]
+__all__ = [
+    '__version__',
+    algo, Algo, CompositeAlgo, AggregateAlgo,
+    metrics, Metrics,
+    opener, Opener
+    ]
