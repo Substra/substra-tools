e73fa725e2a2c186a6712280a885f2286460a6a0
ThibaultFy <50656860+ThibaultFy@users.noreply.github.com>
2022-09-02 15:20:43 +0200

    fix workflow test
    Signed-off-by: Louis Hulot <louis.hulot@owkin.com>

--- tests/test_workflow.py
+++ tests/test_workflow.py
@@ -1,16 +1,18 @@
+import json
 import os
 
 import pytest
 
 from substratools import Algo
 from substratools import Metrics
-from substratools.algo import GenericAlgoWrapper
 from substratools import load_performance
+from substratools import opener
 from substratools import save_performance
+from substratools.algo import GenericAlgoWrapper
 from substratools.metrics import MetricsWrapper
+from substratools.task_resources import TaskResources
 from substratools.utils import import_module
 from substratools.workspace import AlgoWorkspace
-
 from tests import utils
 
 
@@ -63,36 +65,56 @@ class DummyMetrics(Metrics):
 
 
 def test_workflow(workdir, dummy_opener):
+
     a = DummyAlgo()
     loop1_model_path = workdir / "loop1model"
-    loop1_workspace = AlgoWorkspace(output_model_path=str(loop1_model_path))
-    loop1_wp = GenericAlgoWrapper(a, workspace=loop1_workspace)
+    loop1_workspace_outputs = TaskResources(
+        json.dumps([{"id": "model", "value": str(loop1_model_path), "multiple": False}])
+    )
+    loop1_workspace = AlgoWorkspace(outputs=loop1_workspace_outputs)
+    loop1_wp = GenericAlgoWrapper(a, workspace=loop1_workspace, opener_wrapper=None)
 
     # loop 1 (no input)
-    loop1_wp.train()
-    model = utils.load_model(path=loop1_wp._workspace.output_model_path)
+    loop1_wp.task_launcher(method_name="train")
+    model = utils.load_model(path=loop1_wp._workspace.task_outputs["model"])
 
     assert model == {"i": 1, "total": 0}
     assert os.path.exists(loop1_model_path)
 
     loop2_model_path = workdir / "loop2model"
-    loop2_workspace = AlgoWorkspace(input_model_paths=[str(loop1_model_path)], output_model_path=str(loop2_model_path))
-    loop2_wp = AlgoWrapper(a, workspace=loop2_workspace)
+
+    loop2_workspace_inputs = TaskResources(
+        json.dumps([{"id": "model", "value": str(loop1_model_path), "multiple": False}])
+    )
+    loop2_workspace_outputs = TaskResources(
+        json.dumps([{"id": "model", "value": str(loop2_model_path), "multiple": False}])
+    )
+    loop2_workspace = AlgoWorkspace(inputs=loop2_workspace_inputs, outputs=loop2_workspace_outputs)
+    loop2_wp = GenericAlgoWrapper(a, workspace=loop2_workspace, opener_wrapper=None)
 
     # loop 2 (one model as input)
-    loop2_wp.train()
-    model = utils.load_model(path=loop2_wp._workspace.output_model_path)
+    loop2_wp.task_launcher(method_name="train")
+    model = utils.load_model(path=loop2_wp._workspace.task_outputs["model"])
     assert model == {"i": 2, "total": 1}
     assert os.path.exists(loop2_model_path)
 
     loop3_model_path = workdir / "loop2model"
-    loop3_workspace = AlgoWorkspace(
-        input_model_paths=[str(loop1_model_path), str(loop2_model_path)], output_model_path=str(loop3_model_path)
+    loop3_workspace_inputs = TaskResources(
+        json.dumps(
+            [
+                {"id": "model", "value": str(loop1_model_path), "multiple": False},
+                {"id": "model", "value": str(loop2_model_path), "multiple": False},
+            ]
+        )
     )
-    loop3_wp = AlgoWrapper(a, workspace=loop3_workspace)
+    loop3_workspace_outputs = TaskResources(
+        json.dumps([{"id": "model", "value": str(loop3_model_path), "multiple": False}])
+    )
+    loop3_workspace = AlgoWorkspace(inputs=loop3_workspace_inputs, outputs=loop3_workspace_outputs)
+    loop3_wp = GenericAlgoWrapper(a, workspace=loop3_workspace, opener_wrapper=None)
 
     # loop 3 (two models as input)
-    loop3_wp.train()
+    loop3_wp.task_launcher("train")
     model = utils.load_model(path=loop3_wp._workspace.output_model_path)
     assert model == {"i": 3, "total": 3}
     assert os.path.exists(loop3_model_path)
@@ -100,7 +122,7 @@ def test_workflow(workdir, dummy_opener):
     predict_workspace = AlgoWorkspace(
         input_model_paths=[str(loop3_model_path)],
     )
-    predict_wp = AlgoWrapper(a, workspace=predict_workspace)
+    predict_wp = GenericAlgoWrapper(a, workspace=predict_workspace, opener_wrapper=opener.load_from_module())
 
     # predict
     predict_wp.predict()
